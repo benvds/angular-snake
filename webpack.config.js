838a0c00797a5545b1ddcb0b0871ae278943da13
benvds <ben@pomodoro.nl>
2015-12-08 17:44:31 +0100

    extract stylesheets

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,42 @@
 var path = require('path');
-// var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 
-  // set the context (optional)
-  context: __dirname + '/src',
-  entry: './app/app.js',
+    // set the context (optional)
+    context: __dirname + '/src',
+    entry: './app/app.js',
 
-  // enable loading modules relatively
-  resolve: {
-    root: [__dirname + "/src"]
-  },
+        // enable loading modules relatively
+    resolve: {
+        root: [__dirname + "/src"]
+    },
 
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader:"babel" },
-      // { test: /\.scss$/, loader: "style!css!resolve-url!sass?sourceMap"},
-      { test: /\.scss$/, loader: "style!css!resolve-url!sass?sourceMap"},
-      { test: /\.json$/, loader: "json" },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader:"babel" },
+            // { test: /\.scss$/, loader: "style!css!resolve-url!sass?sourceMap"},
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css?sourceMap!resolve-url?sourceMap!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true") },
+            { test: /\.json$/, loader: "json" },
 
-      // load raw html files
-      { test: /\.html$/, exclude: /node_modules/, loader:"raw" },
+            // load raw html files
+            { test: /\.html$/, exclude: /node_modules/, loader:"raw" },
 
-      // load fonts and images
-      { test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/, loader: "file" }
+            // load fonts and images
+            { test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/, loader: "file" }
+        ]
+    },
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, "./node_modules")]
+    },
+
+        // support source maps
+    devtool: "#inline-source-map",
+
+    plugins: [
+        new ExtractTextPlugin("styles.css?[hash]-[chunkhash]-[contenthash]-[name]", {
+            disable: false,
+            allChunks: true
+        })
     ]
-  },
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, "./node_modules")]
-  },
-
-  // support source maps
-  devtool: "#inline-source-map"
 };
